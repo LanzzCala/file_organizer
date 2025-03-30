aaa6c36d8030961a95c19d230869afa6cdd4994b
Lanzz Cala <lanzzcala@gmail.com>
2025-03-30 15:21:44 -0700

    Testing os functions to open directory

--- file_organizer.py
+++ file_organizer.py
@@ -1,3 +1,9 @@
 import os
 
-print (os.getcwd())
+print (f"I am working in this directory. {os.getcwd()}")
+os.chdir("c:\\Users\\lanzz\\OneDrive\\Desktop")
+print(f"Now i will work in this directory: {os.getcwd()}")
+print (os.listdir())
+with open('test files', 'r', encoding="utf-8") as x:
+    read_files = x.read()
+
