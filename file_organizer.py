210e370b17e6526c8fe8ea1f86a87cba190f9617
Lanzz Cala <lanzzcala@gmail.com>
2025-03-30 15:54:41 -0700

    Starting project over from beginning

--- file_organizer.py
+++ file_organizer.py
@@ -1,9 +1,2 @@
 import os
-
-print (f"I am working in this directory. {os.getcwd()}")
-os.chdir("c:\\Users\\lanzz\\OneDrive\\Desktop")
-print(f"Now i will work in this directory: {os.getcwd()}")
-print (os.listdir())
-with open('test files', 'r', encoding="utf-8") as x:
-    read_files = x.read()
-
+import pathlib as path
