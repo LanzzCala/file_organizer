5de9f5e31cd5924f9edc0421ea8b760fa3d11ef7
Lanzz Cala <lanzzcala@gmail.com>
2025-03-30 18:15:14 -0700

    Create dictionary of file types

--- file_organizer.py
+++ file_organizer.py
@@ -1,2 +1,10 @@
 import os
 import pathlib as path
+
+list_of_directories = {
+    "Images_Folder": [".jpeg", ".jpg", ".gif", ".png"],
+    "Videos_Folder": [".wmv", ".mov", ".mp4", "mpg", ".mpeg", ".mkv"],
+    "Zips_Folder": [".iso", ".tar", ".gz", ".rz", ".7z", ".dmg", ".rar", ".zip"],
+    "Music_Folder": [".mp3", ".msv", ".wav", ".wma"],
+    "PDF_Folder": [".pdf"],
+}
