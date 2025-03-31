6fcc9cf246b12fef4470e3c5796fc0b63b49db35
Lanzz Cala <lanzzcala@gmail.com>
2025-03-31 14:12:41 -0700

    Create dictionary with each file format organized. Start organizer function

--- file_organizer.py
+++ file_organizer.py
@@ -8,3 +8,18 @@ list_of_directories = {
     "Music_Folder": [".mp3", ".msv", ".wav", ".wma"],
     "PDF_Folder": [".pdf"],
 }
+
+file_format_dictionary = {
+    final_file_format: directory
+    for directory, file_format_stored in list_of_directories.items()
+    for final_file_format in file_format_stored
+}
+
+def organizer():
+    for entry in os.scandir():
+        if entry.is_dir():
+            continue
+        file_path = path(entry)
+        final_file_format = file_path.suffix.lower()
+        if final_file_format in file_format_dictionary:
+            directory_path = path(file_format_dictionary[final_file_format])
