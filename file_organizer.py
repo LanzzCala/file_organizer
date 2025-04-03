86eac568ecef49643b962ca720bb82edca37d056
Lanzz Cala <lanzzcala@gmail.com>
2025-04-02 23:11:54 -0700

    Create dictionary of common extensions and directories

--- file_organizer.py
+++ file_organizer.py
@@ -4,4 +4,28 @@ from pathlib import Path
 
 directory = Path.home()/ "Downloads"
 
-print (directory)
+extensions = {
+    ".jpg" : "Images",
+    ".png" : "Images",
+    ".jpeg": "Images",
+    ".gif": "Images",
+    ".wmv": "Videos",
+    ".mov": "Videos",
+    ".mp4": "Videos",
+    ".mpg": "Videos",
+    ".mpeg": "Videos",
+    ".mkv": "Videos",
+    ".iso": "Zips",
+    ".tar": "Zips",
+    ".gz": "Zips",
+    ".7z": "Zips",
+    ".dmg": "Zips",
+    ".rar": "Zips",
+    ".zip": "Zips",
+    ".mp3": "Music",
+    ".msv": "Music",
+    ".wave": "Music",
+    ".wma": "Music",
+    ".pdf": "Documents",
+    ".doc": "Documents",
+}
